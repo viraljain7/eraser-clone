4ab88c567926397ba62491a9e3e2e39f35e32a29
viraljain7 <veerljain1234@gmail.com>
2024-02-27 23:21:45 +0530

    Setup: middleware & page protection

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import React from "react";
 
 const page = () => {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      dashboard
+      <Button>
+        <LogoutLink>logout</LogoutLink>
+      </Button>
+    </div>
+  );
 };
 
 export default page;
